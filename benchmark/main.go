d23f6d563af7adb19f55b56447429cee40cecf17
Haya Kawamitsu <halation3@gmail.com>
2022-05-22 14:22:45 +0000

    Update scoring logic

--- benchmark/main.go
+++ benchmark/main.go
@@ -20,12 +20,18 @@ import (
 	"time"
 
 	asset "cloud.google.com/go/asset/apiv1"
+	compute "cloud.google.com/go/compute/apiv1"
+	instance "cloud.google.com/go/spanner/admin/instance/apiv1"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-contrib/timeout"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2/google"
 	"golang.org/x/sync/semaphore"
 	"google.golang.org/api/iterator"
+	"google.golang.org/api/sqladmin/v1"
 	assetpb "google.golang.org/genproto/googleapis/cloud/asset/v1"
+	computepb "google.golang.org/genproto/googleapis/cloud/compute/v1"
+	instancepb "google.golang.org/genproto/googleapis/spanner/admin/instance/v1"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -44,10 +50,13 @@ const (
 	SCORE_POST_CHECKOUT       = 2
 	SCORE_GET_PRODUCT         = 1
 	SCORE_GET_CHECKOUTS       = 4
-	RATE_NO_RESOURCE          = 0
-	RATE_ZONAL                = 1
-	RATE_REGIONAL             = 2
-	RATE_MULTI_REGIONAL       = 3
+)
+
+const (
+	RATE_NO_RESOURCE = iota
+	RATE_ZONAL
+	RATE_REGIONAL
+	RATE_MULTI_REGIONAL
 )
 
 var (
@@ -101,14 +110,6 @@ type UsersBlob struct {
 
 type ImageHashesBlob struct {
 	ImageHashes []ImageHash `json:"image_hashes"`
-}
-
-func init() {
-	queue = make(chan Request, getNumOfUsers())
-	jobInQueue = sync.Map{}
-	imageHashes = initImageHashes()
-	worker = Worker{sem: semaphore.NewWeighted(int64(getLimitOfWorkers())), conn: initDBConn()}
-	users = initUsers()
 }
 
 func initImageHashes() map[string]string {
@@ -652,6 +653,390 @@ func scoreArchitecture(projectID string) (uint, error) {
 	return score, nil
 }
 
+func rateArchitecture(projectID string, labels map[string]string) (int, error) {
+	if len(labels) == 0 {
+		return 0, fmt.Errorf("labels are not set")
+	}
+
+	ac := NewAvailabilityChecker()
+	ac.SetProjectID(projectID)
+
+	minRate := math.MaxInt32
+	rateFunctions := []func(string, string) (int, error){
+		ac.RateComputeEngine,
+		ac.RateAppEngine,
+		ac.RateCloudRun,
+		ac.RateCloudFunctions,
+		ac.RateCloudSQL,
+		ac.RateCloudSpanner,
+		// ac.RateAlloyDB,
+		// ac.RateBigTable,
+		// ac.Datastore,
+		// ac.RateBigQuery,
+	}
+
+	for k, v := range labels {
+		minRateByLabel := math.MaxInt32
+		for _, f := range rateFunctions {
+			rate, err := f(k, v)
+			if err != nil {
+				return 0, err
+			}
+
+			if rate > RATE_NO_RESOURCE {
+				minRateByLabel = getMin(minRateByLabel, rate)
+			}
+		}
+
+		if minRateByLabel == math.MaxInt32 {
+			return 0, fmt.Errorf("Resource labelled %s:%s is not found", k, v)
+		}
+
+		minRate = getMin(minRate, minRateByLabel)
+	}
+
+	return minRate, nil
+}
+
+func rateAvailability(regions map[string]interface{}, zones map[string]interface{}) int {
+	if len(regions) >= 2 {
+		return RATE_MULTI_REGIONAL
+	}
+
+	if len(zones) >= 2 {
+		return RATE_REGIONAL
+	}
+
+	if len(zones) > 0 {
+		return RATE_ZONAL
+	}
+
+	return RATE_NO_RESOURCE
+}
+
+func (ac *AvailabilityChecker) RateComputeEngine(labelKey string, labelVal string) (int, error) {
+	ctx := context.Background()
+	c, err := compute.NewInstancesRESTClient(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer c.Close()
+
+	req := &computepb.AggregatedListInstancesRequest{Project: ac.projectID}
+	it := c.AggregatedList(ctx, req)
+
+	type ComputeEngine struct {
+		Fingerprint string
+		Name        string
+		Zone        string
+		MachineType string
+		Status      string
+	}
+	gces := make(map[string]ComputeEngine)
+
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+
+		for _, instance := range resp.Value.Instances {
+			if instance.GetStatus() == "RUNNING" && instance.Labels[labelKey] == labelVal {
+				// For a custom machine type: zones/zone/machineTypes/custom-CPUS-MEMOR e.g. zones/us-central1-f/machineTypes/custom-4-5120
+				gces[instance.GetFingerprint()] = ComputeEngine{
+					Fingerprint: instance.GetFingerprint(),
+					Name:        instance.GetName(),
+					Zone:        path.Base(instance.GetZone()),
+					MachineType: path.Base(instance.GetMachineType()),
+					Status:      instance.GetStatus(),
+				}
+			}
+		}
+	}
+
+	regions := make(map[string]interface{})
+	zones := make(map[string]interface{})
+	for _, gce := range gces {
+		region := strings.Join(strings.Split(gce.Zone, "-")[0:2], "-")
+		regions[region] = struct{}{}
+		zones[gce.Zone] = struct{}{}
+	}
+
+	return rateAvailability(regions, zones), nil
+}
+
+func (ac *AvailabilityChecker) RateAppEngine(labelKey string, labelVal string) (int, error) {
+	scope := fmt.Sprintf("projects/%s", ac.projectID)
+	ctx := context.Background()
+	client, err := asset.NewClient(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer client.Close()
+
+	req := &assetpb.SearchAllResourcesRequest{
+		Scope: scope,
+		AssetTypes: []string{
+			"appengine.googleapis.com/Application",
+			"appengine.googleapis.com/Service",
+		},
+	}
+
+	it := client.SearchAllResources(ctx, req)
+	isServed, isLabelled := false, false
+	for {
+		resource, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+
+		if resource.GetAssetType() == "appengine.googleapis.com/Application" {
+			isServed = resource.GetState() == "SERVING"
+		}
+
+		if resource.GetAssetType() == "appengine.googleapis.com/Service" {
+			isLabelled = resource.GetLabels()[labelKey] == labelVal
+		}
+	}
+
+	if isServed == isLabelled {
+		return RATE_REGIONAL, nil
+	}
+
+	return RATE_NO_RESOURCE, nil
+}
+
+func (ac *AvailabilityChecker) RateCloudRun(labelKey string, labelVal string) (int, error) {
+	scope := fmt.Sprintf("projects/%s", ac.projectID)
+	ctx := context.Background()
+	client, err := asset.NewClient(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer client.Close()
+
+	req := &assetpb.SearchAllResourcesRequest{
+		Scope: scope,
+		AssetTypes: []string{
+			"run.googleapis.com/Service",
+		},
+	}
+
+	regions := make(map[string]interface{})
+	it := client.SearchAllResources(ctx, req)
+	for {
+		resource, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+		if resource.GetLabels()[labelKey] == labelVal {
+			regions[resource.GetLocation()] = struct{}{}
+		}
+	}
+
+	if len(regions) == 0 {
+		return RATE_NO_RESOURCE, nil
+	}
+
+	if len(regions) >= 2 {
+		return RATE_MULTI_REGIONAL, nil
+	}
+
+	return RATE_REGIONAL, nil
+}
+
+func (ac *AvailabilityChecker) RateCloudFunctions(labelKey string, labelVal string) (int, error) {
+	scope := fmt.Sprintf("projects/%s", ac.projectID)
+	ctx := context.Background()
+	client, err := asset.NewClient(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer client.Close()
+
+	req := &assetpb.SearchAllResourcesRequest{
+		Scope: scope,
+		AssetTypes: []string{
+			"cloudfunctions.googleapis.com/CloudFunction",
+		},
+	}
+
+	regions := make(map[string]interface{})
+	it := client.SearchAllResources(ctx, req)
+	for {
+		resource, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+		if resource.GetLabels()[labelKey] == labelVal {
+			regions[resource.GetLocation()] = struct{}{}
+		}
+	}
+
+	if len(regions) == 0 {
+		return RATE_NO_RESOURCE, nil
+	}
+
+	if len(regions) >= 2 {
+		return RATE_MULTI_REGIONAL, nil
+	}
+
+	return RATE_REGIONAL, nil
+}
+
+// TODO: Consider case of multi-regional instances
+// Use Cloud SQL Admin API because Asset Inventory doesn't reply availability type
+func (ac *AvailabilityChecker) RateCloudSQL(labelKey string, labelVal string) (int, error) {
+	ctx := context.Background()
+
+	// Create an http.Client that uses Application Default Credentials.
+	hc, err := google.DefaultClient(ctx, sqladmin.SqlserviceAdminScope)
+	if err != nil {
+		return 0, err
+	}
+
+	// Create the Google Cloud SQL service.
+	service, err := sqladmin.New(hc)
+	if err != nil {
+		return 0, err
+	}
+
+	// List instances for the project ID.
+	instances, err := service.Instances.List(ac.projectID).Do()
+	if err != nil {
+		return 0, err
+	}
+
+	if len(instances.Items) == 0 {
+		return RATE_NO_RESOURCE, nil
+	}
+
+	type CloudSQL struct {
+		Name             string
+		AvailabilityType string
+		Status           string
+		MachineType      string
+	}
+
+	sqls := make(map[string]CloudSQL)
+	for _, instance := range instances.Items {
+		if instance.State == "RUNNABLE" && instance.Settings.UserLabels[labelKey] == labelVal {
+			sqls[instance.Name] = CloudSQL{
+				Name:             instance.Name,
+				AvailabilityType: instance.Settings.AvailabilityType,
+				Status:           instance.State,
+				MachineType:      instance.Settings.Tier,
+			}
+		}
+	}
+
+	if len(sqls) == 0 {
+		return RATE_NO_RESOURCE, nil
+	}
+
+	for _, sql := range sqls {
+		if sql.AvailabilityType == "REGIONAL" {
+			return RATE_REGIONAL, nil
+		}
+	}
+
+	return RATE_ZONAL, nil
+}
+
+// In order to get processing_units metrics, sdk library is required as Asset Inventory can't collect.
+func (ac *AvailabilityChecker) RateCloudSpannerSDK(labelKey string, labelVal string) (int, error) {
+	ctx := context.Background()
+	c, err := instance.NewInstanceAdminClient(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer c.Close()
+
+	req := &instancepb.ListInstancesRequest{
+		Parent: fmt.Sprintf("projects/%s", ac.projectID),
+	}
+	it := c.ListInstances(ctx, req)
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+		// TODO: Use resp.
+		fmt.Println(resp)
+	}
+
+	return 0, nil
+}
+
+func (ac *AvailabilityChecker) RateCloudSpanner(labelKey string, labelVal string) (int, error) {
+	scope := fmt.Sprintf("projects/%s", ac.projectID)
+	ctx := context.Background()
+	client, err := asset.NewClient(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer client.Close()
+
+	req := &assetpb.SearchAllResourcesRequest{
+		Scope: scope,
+		AssetTypes: []string{
+			"spanner.googleapis.com/Instance",
+		},
+	}
+
+	regions := make(map[string]interface{})
+	it := client.SearchAllResources(ctx, req)
+	for {
+		resource, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+
+		if err != nil {
+			return 0, err
+		}
+
+		if resource.GetLabels()[labelKey] == labelVal {
+			location := resource.GetLocation()
+			if !strings.Contains(location, "regional") {
+				return RATE_MULTI_REGIONAL, nil
+			}
+			regions[location] = struct{}{}
+		}
+	}
+
+	if len(regions) == 0 {
+		return RATE_NO_RESOURCE, nil
+	}
+
+	return RATE_REGIONAL, nil
+}
+
+// TODO: Call AlloyDB API directly as client library doesn't support the instance management
+// https://cloud.google.com/alloydb/docs/reference/rest
+// func (ac *AvailabilityChecker) RateAlloyDB(labelKey string, labelVal string) (int, error) {
+// 	return 0, nil
+// }
+
+// func (ac *AvailabilityChecker) RateBigTable() (int, error) {
+// 	return 0, nil
+// }
+
 func (w Worker) RunScoring() {
 	w.sem.Acquire(context.Background(), WEIGHT_OF_WORKER)
 	defer w.sem.Release(WEIGHT_OF_WORKER)
@@ -689,13 +1074,20 @@ func (w Worker) RunScoring() {
 	}
 	result.BenchScore = benchScore
 
-	pfRate, err := scoreArchitecture(job.ProjectID)
+	labels := map[string]string{
+		"service_role_webapp": "true",
+		"service_role_db":     "true",
+	}
+
+	pfRate, err := rateArchitecture(job.ProjectID, labels)
+
+	// pfRate, err := scoreArchitecture(job.ProjectID)
 	if err != nil {
 		result.PlatformResultMsg = err.Error()
 	}
-	result.PlatformRate = pfRate
+	result.PlatformRate = uint(pfRate)
 
-	result.TotalScore = benchScore * pfRate
+	result.TotalScore = result.BenchScore * result.PlatformRate
 
 	// Store the result in the database server
 	if err := w.conn.Create(&result).Error; err != nil {
@@ -786,6 +1178,12 @@ func getEnvPort() int {
 }
 
 func main() {
+	queue = make(chan Request, getNumOfUsers())
+	jobInQueue = sync.Map{}
+	imageHashes = initImageHashes()
+	worker = Worker{sem: semaphore.NewWeighted(int64(getLimitOfWorkers())), conn: initDBConn()}
+	users = initUsers()
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 	r.StaticFile("/favicon.ico", "favicon.ico")
